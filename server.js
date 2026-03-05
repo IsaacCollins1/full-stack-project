3442f9d4ca42783097b8834bdf03d20e916d0919
IsaacCollins1 <isaaccollins701@gmail.com>
2026-03-05 11:13:22 +0000

    Database working

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ app.use(cors())
 app.use(express.json())
 const db = new sqlite3.Database("./players.db")
 
+db.run(`
+CREATE TABLE IF NOT EXISTS players (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT,
+position TEXT,
+number INTEGER
+)
+`)
+
+db.run(`INSERT INTO players (name, position, number) VALUES ('Harry Kane', 'Striker', 9)`)
+db.run(`INSERT INTO players (name, position, number) VALUES ('Jude Bellingham', 'Midfielder', 22)`)
+db.run(`INSERT INTO players (name, position, number) VALUES ('Kyle Walker', 'Defender', 2)`)
+
+
 app.listen(3000, () => {
   console.log("Server running on port 3000")
 })
+
