bd35c116e6ae6feeb7c973fa627d0938c6ea3310
Dolapo265 <dolaposalaam@outlook.com>
2026-03-05 11:27:06 +0000

    API endpoint

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require("express")
-const cors = require("cors")
-const sqlite3 = require("sqlite3").verbose()
-const app = express()
-app.use(cors())
-app.use(express.json())
-const db = new sqlite3.Database("./players.db")
+const express = require("express");
+const cors = require("cors");
+const sqlite3 = require("sqlite3").verbose();
+const app = express();
+app.use(cors());
+app.use(express.json());
+const db = new sqlite3.Database("./players.db");
 
 db.run(`
 CREATE TABLE IF NOT EXISTS players (
@@ -13,14 +13,35 @@ name TEXT,
 position TEXT,
 number INTEGER
 )
-`)
+`);
 
-db.run(`INSERT INTO players (name, position, number) VALUES ('Harry Kane', 'Striker', 9)`)
-db.run(`INSERT INTO players (name, position, number) VALUES ('Jude Bellingham', 'Midfielder', 22)`)
-db.run(`INSERT INTO players (name, position, number) VALUES ('Kyle Walker', 'Defender', 2)`)
+db.run(
+  `INSERT INTO players (name, position, number) VALUES ('Harry Kane', 'Striker', 9)`,
+);
+db.run(
+  `INSERT INTO players (name, position, number) VALUES ('Jude Bellingham', 'Midfielder', 22)`,
+);
+db.run(
+  `INSERT INTO players (name, position, number) VALUES ('Kyle Walker', 'Defender', 2)`,
+);
 
+app.get("/players", (req, res) => {
+  db.all("SELECT * FROM players", [], (err, rows) => {
+    res.json(rows);
+  });
+});
+
+app.post("/players", (req, res) => {
+  const { name, position, number } = req.body;
+  db.run(
+    "INSERT INTO players (name, position, number) VALUES (?, ?, ?)",
+    [name, position, number],
+    function (err) {
+      res.json({ id: this.lastID });
+    },
+  );
+});
 
 app.listen(3000, () => {
-  console.log("Server running on port 3000")
-})
-
+  console.log("Server running on port 3000");
+});
