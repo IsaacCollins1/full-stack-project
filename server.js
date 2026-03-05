e575f6724e0a1368184e56910b5e767a3e6f51f8
IsaacCollins1 <isaaccollins701@gmail.com>
2026-03-05 11:01:25 +0000

    Added Core stuff

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const cors = require("cors")
+const sqlite3 = require("sqlite3").verbose()
+const app = express()
+app.use(cors())
+app.use(express.json())
+const db = new sqlite3.Database("./players.db")
